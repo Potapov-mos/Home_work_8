fe096139be7f429695b9009f3e27f59ad88b6eca
Dmitriy <potapidze.mos@mail.ru>
2022-11-14 16:57:42 +0300

    Добавил решение задачи №54

--- Task_54/Program.cs
+++ Task_54/Program.cs
@@ -13,3 +13,58 @@
 // 8 4 4 2
 
 
+// Заполнение массива рандомными числами от 1 до 9
+
+void CreateMatrixRndInt(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+// Вывод двумерного массива
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+// Упорядочивание элементов в строке двумерного массива по убыванию
+
+void SortElem(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+int[,] table = new int[3, 4];
+CreateMatrixRndInt(table);
+Console.WriteLine();
+PrintMatrix(table);
+SortElem(table);
+Console.WriteLine();
+PrintMatrix(table);
